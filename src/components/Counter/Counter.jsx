bcc2e35e3e66ab82da10997b89e21a7676aca727
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-23 10:03:36 +0600

    Final Code

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,20 +1,24 @@
 import React, { useMemo, useState } from 'react'
 
 const Counter = () => {
-    const [plus, setPlus] = useState(0)
-    const [minus, setMinus] = useState(10)
+    // ================ useState Hook============
 
+    const [plus, setPlus] = useState(0);
+    const [minus, setMinus] = useState(10);
+  
+    // ================ Functions============
     const handlePlus = () => {
-        setPlus((plus + 1))
+        setPlus((plus + 1));
     }
     const handleMinus = () => {
-        setMinus((minus - 1))
+        setMinus((minus - 1));
     }
-
+    // ================ useMemo Hook============
     const checking = useMemo(() => {
         console.log("For Cheacking");
         return plus % 2 === 0;
     }, [plus]);
+
     return (
         <div >
             <h1> {checking ? "Even" : "Odd"} </h1>
