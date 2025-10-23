ba2fb2365fb5a68fa98e219892a0458ec6957ab9
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-23 09:58:24 +0600

    update -repo

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -10,14 +10,14 @@ const Counter = () => {
     const handleMinus = () => {
         setMinus((minus - 1))
     }
-  const checking = useMemo(() =>{
-console.log("Checking");
 
-    return plus % 2 === 0;
-  })
+    const checking = useMemo(() => {
+        console.log("For Cheacking");
+        return plus % 2 === 0;
+    }, [plus]);
     return (
         <div >
-            <h1> {checking() ? "Even" : "Odd"} </h1>
+            <h1> {checking ? "Even" : "Odd"} </h1>
             <button onClick={handlePlus} className='m-10'>  Increment  :   {plus} </button>
             <hr />
             <br />
